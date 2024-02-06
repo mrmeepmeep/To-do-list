c4e4d35d3195fef59512029419dd694455de662e
mrmeepmeep <myachou42@gmail.com>
2024-02-06 23:42:15 +0100

    Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import './style.css'
-import createForm from './createForm.js'
-
-const dialogBtn = document.querySelector('#dialogBtn')
-const closeBtn = document.querySelector("#closeBtn")
-const dialog = document.querySelector("dialog")
-
-createForm()
+import { createTodo, library } from './createTodo'
+import { generateForm } from './createForm'
 
 
-dialogBtn.addEventListener("click",() => {
-dialog.showModal()
-})
 
-closeBtn.addEventListener("click", () => {
-    dialog.close()
-})
+
+
+
+
+
+
